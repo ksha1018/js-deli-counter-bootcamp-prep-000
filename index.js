ee2b05bdc6e9e8dc33d654b2747057a387c9d7cf
ksha1018@naver.com <ksha1018@naver.com>
2018-03-03 01:52:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var katzDeli = [];
 
 function takeANumber(array1, name){
   array1[array1.length] = name;
-  console.log("Welcome, ${name} You are number "+array1.length+"" in line.")
+  console.log("Welcome, ${name} You are number "+array1.length+" in line.")
   return array1;
 }
 
-console(katzDeli, "kwon");
+console.log(takeANumber(katzDeli, "kwon"));
